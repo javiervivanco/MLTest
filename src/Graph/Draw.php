ab09b9036506e791d26d4e1f4f77cc9e86124a8c
javier vivanco <javier.vivanco@teracode.com>
2016-03-18 16:43:30 -0300

    version final

--- src/Graph/Draw.php
+++ src/Graph/Draw.php
@@ -15,23 +15,36 @@ class Draw {
         $pixel['r'] = new \ImagickPixel('red'); 
         $pixel['g'] = new \ImagickPixel('green'); 
         $pixel['b'] = new \ImagickPixel('blue'); 
-        $image->newImage(5000, 5000, 'black', 'gif'); 
+        $width    = 400;
+        $height   = 400;
+        $width_2  = $width/2;
+        $offset   = 0.5;
+        $width_2u = $width_2+$offset;
+        $width_2d = $width_2-$offset;
+        $height_2 = $height/2;
+        $height_2u=$height_2+$offset;
+        $height_2d=$height_2-$offset;
+
+        $zoom   = 13;
+        $image->newImage($width, $height, 'grey', 'gif'); 
 
         // Here comes the magick: 
         $name = '';
         $draw->setFillColor($pixel['y']); 
         $planets = $this->sun->getPlanets();
-        $draw->circle(2495,2495,2505,2505); 
+        $draw->circle($width_2d-2,$height_2d-2,$width_2u+2,$height_2u+2); 
         foreach ($planets as $planet) {
             $draw->setFillColor(next($pixel)); 
             $pos = $planet->getPositionCartesianByDay($day);
             $name .= $planet->getName();
-            $draw->circle($pos->x+2495,$pos->y+2495,$pos->x+2505,$pos->y+2505 ); 
+            $draw->circle($pos->x/$zoom+$width_2d,$pos->y/$zoom+$height_2d,$pos->x/$zoom+$width_2u,$pos->y/$zoom+$height_2u ); 
         }
         $name .= '_'.$day;
         $image->drawImage($draw); 
-        $image->writeImages(sprintf($path, $name), true); 
+        $file = sprintf($path, $name);
+        $image->writeImages($file, true); 
         $image->clear();
+        return $file;
     }
 
     public function drawRangeDays($from_day, $to_day){
--- web/app.php
+++ web/app.php
@@ -5,41 +5,81 @@ require '../vendor/autoload.php';
 use PlanetarySystem\Sun;
 use PlanetarySystem\Planet;
 use Weather\Weather;
+use Graph\Draw;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 $sun       = new Sun();
 $planet_1  = new Planet('Ferengi',   $sun, deg2rad(1), 500, true);
 $planet_2  = new Planet('Betasoide', $sun, deg2rad(5), 1000, true);
 $planet_3  = new Planet('Vulcano',   $sun, deg2rad(3), 2000, false);   
 $weather   = new Weather($sun);
+$draw      = new Draw($sun);
+
+function weather_label($clima_dia){
+    switch ($clima_dia) {
+    case Weather::RAINY:
+        return 'Lluvioso';
+        break;                    
+    case Weather::MAX_RAINY:
+        return 'Pico maximo de lluvias';
+        break;
+    case Weather::DROUGHT:
+        return 'Sequia';
+        break;
+    case Weather::OPTIMUS:
+        return 'Condiciones optimas';
+        break;                
+    case Weather::NORMAL:
+        return 'Normal';
+        break;
+}
+
+}
 
 $app = new Silex\Application(); 
+$app['debug'] = true;
+$app->register(new Silex\Provider\UrlGeneratorServiceProvider());
 
 $app->get('/', function() use($app) {
+    return file_get_contents("homepage.html");
+}) ;
+
+
+
+$app->get('/pronostico_extendido', function() use($app, $weather) {
+    $stream = function () use ($weather){
+        echo sprintf("Día, Estado\n");
+        for ($i=0; $i <= 3600; $i++) { 
+            echo sprintf("%s, %s\n", $i, weather_label($weather->getByDay($i)));
+        }        
+    };
+    $h['Content-Type'] = 'text/csv';
+    $h['Content-Disposition'] = 'attachment; filename=pronostico_extendido.csv';
+    return $app->stream($stream, 200, $h);
 
 }) ;
 
 $app->get('/clima/{dia}', function($dia) use($app, $weather) { 
     $clima_dia = $weather->getByDay($dia);
-    switch ($clima_dia) {
-        case Weather::RAINY:
-            $clima_dia='Lluvioso';
-            break;                    
-        case Weather::MAX_RAINY:
-            $clima_dia='Pico maximo de lluvias';
-            break;
-        case Weather::DROUGHT:
-            $clima_dia='Sequia';
-            break;
-        case Weather::OPTIMUS:
-            $clima_dia='Condiciones optimas';
-            break;                
-        case Weather::NORMAL:
-            $clima_dia='Normal';
-            break;
-    }
     $clima['dia'] = $dia;
-    $clima['clima'] = $clima_dia;
+    $clima['clima'] = weather_label($clima_dia);
+    $clima['graph'] = $app['url_generator']
+        ->generate('graph',array('dia'=> $dia), UrlGeneratorInterface::ABSOLUTE_URL);
     return $app->json($clima);
 
-}); 
+});
+
+$app->get('/clima/ver/{dia}', function($dia) use($app, $draw) { 
+    $file=$draw->drawADay($dia);
+    
+    if (!file_exists($file)) {
+        return $app->abort(404, 'The image was not found.');
+    }
+
+    $stream = function () use ($file) {
+        readfile($file);
+    };
+    return $app->stream($stream, 200, array('Content-Type' => 'image/gif'));
+
+})->bind('graph');; 
 
 $app->run(); 
